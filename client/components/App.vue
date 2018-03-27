ebfb5cc8f1b0a48aefee64ea143314878aa5a8ce
Michal Bujalski <michal@tsh.io>
2018-03-27 20:12:48 +0200

    Basic Bulma customisation

--- client/components/App.vue
+++ client/components/App.vue
@@ -5,6 +5,35 @@
 </template>
 <style lang="scss">
 @import '../style/_vars.scss';
+@import "~bulma/sass/utilities/_all";
+// Set your colors
+$primary: $bc-green-medium-1;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099ff;
+$twitter-invert: findColorInvert($twitter);
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+    "white": ($white, $black),
+    "black": ($black, $white),
+    "light": ($light, $light-invert),
+    "dark": ($dark, $dark-invert),
+    "primary": ($primary, $primary-invert),
+    "info": ($info, $info-invert),
+    "success": ($success, $success-invert),
+    "warning": ($warning, $warning-invert),
+    "danger": ($danger, $danger-invert),
+    "twitter": ($twitter, $twitter-invert)
+);
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+
+// Set your colors
+$primary: #ff0000;
 body {
   margin: 0;
   font-size: 2rem;
@@ -15,5 +44,6 @@ body {
                'segoe ui', arial,
                sans-serif;
   background-color: $page-bg;
+  padding: $page-padding;
 }
 </style>
