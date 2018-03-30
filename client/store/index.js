6aace14537f241f45e4264dde1572f1b12170a15
Michal Bujalski <michal@tsh.io>
2018-03-30 13:03:27 +0200

    Added default page state

--- client/store/index.js
+++ client/store/index.js
@@ -4,10 +4,46 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  count: 0
+  newField: {
+    type: 1,
+    displayLabel: '',
+    defaultValue: '',
+    validationRegex: '',
+    tags: [],
+    groups: []
+  },
+  fieldTypes: [
+    {
+      id: 1,
+      icon: 'icon-text',
+      name: 'Text',
+      definition: 'String of text',
+      defaultDisplay: 'Free-form text input'
+    },
+    {
+      id: 2,
+      icon: 'icon-date',
+      name: 'Date',
+      definition: 'Standard ISO format date',
+      defaultDisplay: 'Datepicker, with configurable format'
+    },
+    {
+      id: 3,
+      icon: 'icon-car',
+      name: 'VIN',
+      definition: 'Vehicle Identification Number',
+      defaultDisplay: 'Free-form text input'
+    }
+  ],
+  tagGroups: [
+
+  ]
 }
 
 const mutations = {
+  setFieldType (state, { id }) {
+    Vue.set(state.newField, 'type', id)
+  },
   INCREMENT (state) {
     state.count++
   },
