94b83a3b11602c8c3fa50164ff0958a894ef9736
Michal Bujalski <bujalski.michal@gmail.com>
2018-04-02 13:13:28 +0200

    Removed default regex

--- client/components/FieldDetails.vue
+++ client/components/FieldDetails.vue
@@ -73,7 +73,7 @@ export default {
     return {
       displayLabel: '',
       defaultValue: '',
-      validationRegex: '^([0-9]+)$',
+      validationRegex: '',
       referenceName: '',
       selectedTags: [],
       selectedTagGroups: []
