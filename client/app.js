b5cffb78ed82225ca7cfceba2698d5db7ab73865
Michal Bujalski <bujalski.michal@gmail.com>
2018-04-02 12:18:14 +0200

    Added vee-validation library

--- client/app.js
+++ client/app.js
@@ -4,8 +4,11 @@ import App from './components/App'
 import router from './router'
 import store from './store'
 import Buefy from 'buefy'
+import VeeValidate from 'vee-validate'
+
 sync(store, router)
 Vue.use(Buefy)
+Vue.use(VeeValidate)
 const app = new Vue({
   router,
   store,
